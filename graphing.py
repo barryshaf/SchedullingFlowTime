2f0bcba7d2f445b9a9e59a3b3ad30554f1bddedb
physmatic <83124492+physmatic@users.noreply.github.com>
2025-05-18 15:37:34 +0300

    sababa
    sababa

--- graphing.py
+++ graphing.py
@@ -1,5 +1,5 @@
 """
-Module for graphing the results of lamdscape calculations and VQE experiments.
+Module for graphing the results of landscape calculations and VQE experiments.
 """
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -29,7 +29,6 @@ def display_energy_landscape(energy_landscape_results: TotalLandscapeResult, gra
     for i, mub_res in enumerate(energy_landscape_results.mub_results):
         for j, subset_res in enumerate(mub_res):
             energies_only = [result.value for result in subset_res]
-            #print(energies_only)
             # For plots with full MUBs (no different subsets in each basis), have a distinct color for each basis.
             # For plots with partial MUBs (several subsets in each basis), keep distinct bases 
             state_color = f"C{i}" if len(mub_res) == 1 else f"C{j}"
